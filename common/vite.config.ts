a110b5d16de80a9657105346b3bf20ab34b39d51
wangchao0724 <wc19950724@gmail.com>
2024-07-15 22:11:56 +0800

    refactor(vite-config): optimize chunk naming and resolve alias
    Optimize the chunk naming strategy in vite.config.ts by refining the
    chunkFileNames function to directly generate the desired output path
    without additional string manipulation. Also, resolve alias now
    properly handles different scenarios for better module resolution.
    
    The changes include moving common configuration functions to a separate
    file, modularizing the configuration, and ensuring that the TypeScript
    compilation output directory is correctly set up for type definitions.

--- common/vite.config.ts
+++ common/vite.config.ts
@@ -24,85 +24,88 @@ const externalDependencies = [
   ...getDependencies(rootPkg),
   ...getDependencies(pkg)
 ];
-export const defineConfig = (config?: UserConfig | UserConfigFnObject) => {
-  const baseConfig: UserConfig = {
-    build: {
-      lib: {
-        entry: "./src/index.ts"
-      },
-      rollupOptions: {
-        external: externalDependencies,
-        output: [
-          {
-            dir: "dist/es",
-            entryFileNames: "[name].js",
-            chunkFileNames({ name }) {
-              const normalizedPath = path.normalize(name);
 
-              // 去除文件扩展名
-              const baseName = path.basename(
-                normalizedPath,
-                path.extname(normalizedPath)
-              );
-              const dirName = path.dirname(normalizedPath);
-              const chunkPath = path.join(dirName, baseName);
+function customizer(objValue: unknown, srcValue: unknown) {
+  if (isArray(objValue)) {
+    return objValue.concat(srcValue);
+  }
+}
 
-              return `${chunkPath}.js`;
-            },
-            manualChunks(id: string) {
-              const url = path.normalize(id);
-              const searchString1 = `node_modules${path.sep}`;
-              const nodeModulesIndex = url.lastIndexOf(searchString1);
+const baseConfig: UserConfig = {
+  build: {
+    lib: {
+      entry: "./src/index.ts"
+    },
+    rollupOptions: {
+      external: externalDependencies,
+      output: [
+        {
+          dir: "dist/es",
+          entryFileNames: "[name].js",
+          chunkFileNames({ name }) {
+            const normalizedPath = path.normalize(name);
 
-              if (nodeModulesIndex !== -1) {
-                const relativePath = url.slice(
-                  nodeModulesIndex + searchString1.length
-                );
-                const dirs = relativePath.split(path.sep);
-                return `vendor${path.sep}${path.join(...dirs)}`;
-              }
+            // 去除文件扩展名
+            const baseName = path.basename(
+              normalizedPath,
+              path.extname(normalizedPath)
+            );
+            const dirName = path.dirname(normalizedPath);
+            const chunkPath = path.join(dirName, baseName);
 
-              const searchString2 = `src${path.sep}`;
-              const srcIndex = url.indexOf(searchString2);
-              if (srcIndex !== -1) {
-                const relativePath = url.slice(srcIndex + searchString2.length); // 获取 src 后面的路径
-                const dirs = relativePath.split(path.sep);
-                return path.join(...dirs);
-              }
-            }
+            return `${chunkPath}.js`;
           },
-          {
-            format: "umd",
-            name: "Global",
-            dir: "dist/umd",
-            entryFileNames: "[name].js",
-            globals: (name) => {
-              // 根据模块名称转换为大写形式
-              const parts = name.split("/");
-              const moduleName = parts[parts.length - 1]; // 获取模块名部分
-              return moduleName.charAt(0).toUpperCase() + moduleName.slice(1);
+          manualChunks(id: string) {
+            const url = path.normalize(id);
+            const searchString1 = `node_modules${path.sep}`;
+            const nodeModulesIndex = url.lastIndexOf(searchString1);
+
+            if (nodeModulesIndex !== -1) {
+              const relativePath = url.slice(
+                nodeModulesIndex + searchString1.length
+              );
+              const dirs = relativePath.split(path.sep);
+              return `vendor${path.sep}${path.join(...dirs)}`;
+            }
+
+            const searchString2 = `src${path.sep}`;
+            const srcIndex = url.indexOf(searchString2);
+            if (srcIndex !== -1) {
+              const relativePath = url.slice(srcIndex + searchString2.length); // 获取 src 后面的路径
+              const dirs = relativePath.split(path.sep);
+              return path.join(...dirs);
             }
           }
-        ]
-      }
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src")
-      }
+        },
+        {
+          format: "umd",
+          name: "Global",
+          dir: "dist/umd",
+          entryFileNames: "[name].js",
+          globals: (name) => {
+            // 根据模块名称转换为大写形式
+            const parts = name.split("/");
+            const moduleName = parts[parts.length - 1]; // 获取模块名部分
+            return moduleName.charAt(0).toUpperCase() + moduleName.slice(1);
+          }
+        }
+      ]
     }
-  };
-  function customizer(objValue: unknown, srcValue: unknown) {
-    if (isArray(objValue)) {
-      return objValue.concat(srcValue);
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
     }
   }
+};
 
+export function defineConfig(config?: UserConfig): UserConfig;
+export function defineConfig(config?: UserConfigFnObject): UserConfigFnObject;
+export function defineConfig(config?: UserConfig | UserConfigFnObject) {
   if (config instanceof Function) {
-    return ViteDefineConfig((env) => {
-      return mergeWith({}, baseConfig, config(env), customizer);
+    return ViteDefineConfig(function (...args) {
+      return mergeWith({}, baseConfig, config(...args), customizer);
     });
   }
-  const merged = mergeWith({}, baseConfig, config, customizer);
-  return ViteDefineConfig(merged);
-};
+  return ViteDefineConfig(mergeWith({}, baseConfig, config, customizer));
+}
--- packages/react-components/vite.config.ts
+++ packages/react-components/vite.config.ts
@@ -1,16 +1,13 @@
 import inject from "@rollup/plugin-inject";
-import { UserConfig } from "vite";
 import injectCSS from "vite-plugin-css-injected-by-js";
 
 import { defineConfig } from "../../common/vite.config";
 
-const config: UserConfig = {
+export default defineConfig({
   plugins: [
     inject({
       React: "react"
     }),
     injectCSS()
   ]
-};
-
-export default defineConfig(config);
+});
